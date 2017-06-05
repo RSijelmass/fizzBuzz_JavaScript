4a8c6a42da6b1a8c09813d0c44d7d7f46f34b615
RSijelmass <R.Sijelmass@gmail.com>
2017-06-05 17:16:41 +0100

    First test: fizzbuzz returns 'fizz' when given 3

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,9 @@
+describe("FizzBuzz", function() {
+	it('should print "fizz" when the number 3 is given', function() {
+		expect(fizzBuzz([3])).toEqual(["fizz"])
+	});
+
+	it('', function() {
+	
+	});
+});
